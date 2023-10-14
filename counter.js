ff502b046c97b2db48339fb1642501384bc85684
Priyamkatiyarlu <priyamktr7@gmail.com>
2023-10-15 00:17:30 +0530

    added counter button

--- counter.js
+++ counter.js
@@ -9,3 +9,15 @@ const decrement = () => {
     value-=1;
     val.innerText = value;
 };
+const counter = ()=>{
+    let value= parseInt(val.innerText);
+    if(value==0){
+        clearInterval(id);
+        console.log("clearinterval");
+        return;
+    }
+    value-=1;
+    val.innerText = value;
+}
+
+const id = setInterval(counter,1000);
